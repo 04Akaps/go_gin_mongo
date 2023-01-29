07f702a81d792f8a94146aeb772692018f3651d9
유호진 <hojin.yu@supercat.co.kr>
2023-01-29 19:46:45 +0900

    (end) simple mongo CRUD -> mysql과는 다른 구조

--- services/user.service.impl.go
+++ services/user.service.impl.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gin_mongo/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type UserServiceImpl struct {
@@ -20,21 +21,61 @@ func NewUserService(usercollection *mongo.Collection, ctx context.Context) UserS
 
 func (u *UserServiceImpl) CreateUser(user *models.User) error {
 	_, err := u.usercollection.InsertOne(u.ctx,user)
-	return nil
+	return err
 }
 
 func (u *UserServiceImpl) GetUser(name *string) (*models.User, error) {
-	return nil, nil
+	var user *models.User
+	query := bson.D{bson.E{Key : "name", Value : name}}
+	err := u.usercollection.FindOne(u.ctx, query).Decode(&user)
+	return user, err
 }
 
 func (u *UserServiceImpl) GetAllUsers() ([]*models.User, error) {
-	return nil, nil
+	var users []*models.User
+
+	cursor, err := u.usercollection.Find(u.ctx, bson.D{{}})
+
+	if err != nil {
+		return nil, err
+	}
+	
+	for cursor.Next(u.ctx) {
+		var user models.User
+		err := cursor.Decode(&user)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	cursor.Close(u.ctx)
+
+	if len(users) == 0 {
+		return nil, errors.New("documents not found")
+	}
+	return users, nil
 }
 
 func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+	filter := bson.D{primitive.E{Key: "name", Value: user.Name}}
+	update := bson.D{primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "name", Value: user.Name}, primitive.E{Key: "age", Value: user.Age}, primitive.E{Key: "address", Value: user.Address}}}}
+	result, _ := u.usercollection.UpdateOne(u.ctx, filter, update)
+	if result.MatchedCount != 1 {
+		return errors.New("no matched document found for update")
+	}
 	return nil
 }
 
 func (u *UserServiceImpl) DeleteUser(name *string) error {
-
+	filter := bson.D{primitive.E{Key: "name", Value: name}}
+	result, _ := u.usercollection.DeleteOne(u.ctx, filter)
+	if result.DeletedCount != 1 {
+		return errors.New("no matched document found for delete")
+	}
+	return nil
 }
